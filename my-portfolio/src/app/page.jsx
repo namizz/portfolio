9add4f60610b7d65225fa5b021129774045ddb0b
namizz <139370392+namizz@users.noreply.github.com>
2025-04-27 20:24:03 +0300

    contact update

--- my-portfolio/src/app/page.jsx
+++ my-portfolio/src/app/page.jsx
@@ -1,4 +1,3 @@
-import NavBar from "../component/NavBar";
 import Achivement from "../component/container/Achivement";
 import Skill from "../component/container/Skill";
 import Contact from "../component/Contact";
@@ -6,14 +5,11 @@ import Project from "../component/container/ProjectSection";
 import Main from "../component/container/Home";
 import Background from "../component/container/Background";
 import NavbarDemo from "../component/container/NavContainer";
-import FloatingDockDemo from "../component/container/Footer";
 
 export default function Home() {
   return (
     <div className="">
-      {/* <div className="fixed w-full z-50 top-0"> */}
       <NavbarDemo />
-      {/* <NavBar /> */}
 
       <Background>
         <div className="max-w-[1100px] w-full mx-auto pb-10 pt-30 px-4 lg:px-6 xl:px-0">
@@ -40,7 +36,6 @@ export default function Home() {
           </div>
         </div>
       </Background>
-      <FloatingDockDemo />
     </div>
   );
 }
